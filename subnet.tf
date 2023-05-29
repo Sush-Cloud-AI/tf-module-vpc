a971e1f31e8a1b1ad237d4f1a1d842f1e17d85f8
Sush <sushcloud12@gmail.com>
2023-05-29 19:54:56 +0100

    i

---
+++ subnet.tf
@@ -0,0 +1,12 @@
+resource "aws_subnet" "main" {
+count = length(var.SUBNET_CIDR)
+vpc_id     = aws_vpc.main.id
+cidr_block = element(var.SUBNET_CIDR, count.index)
+availability_zone = element(var.AZ, count.index)
+  tags = {
+    Name = "Main"
+  }
+}
+
+## number of subnet to be created will be based on the nuber of SUBNET_CIDR
+## logic: count the length of var and loop it using COUNT and length function
