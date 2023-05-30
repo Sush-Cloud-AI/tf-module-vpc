8bc005a8ca684bc3d7c3701e59ea01749f1fc29a
Sush <sushcloud12@gmail.com>
2023-05-30 14:25:41 +0100

    i

--- routetable.tf
+++ routetable.tf
@@ -19,4 +19,9 @@ resource "aws_route_table_association" "rt-assoc" {
     route_table_id = aws_route_table.route-table.id
 
     }
-  
+
+resource "aws_route" "r" {
+  route_table_id            = var.DEFAULT_VPC_RT
+  destination_cidr_block    = var.VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
