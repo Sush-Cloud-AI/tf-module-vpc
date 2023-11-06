2cd30b46d3937b7de2ace792f64a9af6f5351a29
Sush <sushcloud12@gmail.com>
2023-11-06 12:44:43 +0000

    i

--- subnet.tf
+++ subnet.tf
@@ -9,3 +9,13 @@ tags = {
 }
 }
   
+resource "aws_subnet" "private_subnet" {
+count = length(var.PRIVATE_SUBNET_CIDR)
+vpc_id     = aws_vpc.main.id
+cidr_block = element(var.PRIVATE_SUBNET_CIDR, count.index)
+availability_zone = element(var.AZ, count.index)
+
+tags = {
+    Name = "roboshop-${var.ENV}-priv-subnet-${element(var.AZ, count.index)}"
+}
+}
