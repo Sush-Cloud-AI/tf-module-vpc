5a52d058f0aee32e003f6383df83d61f0398f2d7
Sush <sushcloud12@gmail.com>
2023-11-06 11:57:19 +0000

    i

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
-variable "VPC_CIDR" {
-  
-}
+variable "VPC_CIDR" {}
 
-variable "ENV" {
-  
-}
+variable "ENV" {}
+
+variable "AZ" {}
+variable "PUBLIC_SUBNET_CIDR" {}
+variable "PRIVATE_SUBNET_CIDR" {}
