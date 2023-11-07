dbef81b852373d78142d2373326dbd78064fd9d8
Sush <sushcloud12@gmail.com>
2023-11-07 12:18:16 +0000

    i

--- subnet.tf
+++ subnet.tf
@@ -3,7 +3,8 @@ count = length(var.PUBLIC_SUBNET_CIDR)
 vpc_id     = aws_vpc.main.id
 cidr_block = element(var.PUBLIC_SUBNET_CIDR, count.index)
 availability_zone = element(var.AZ, count.index)
-
+map_public_ip_on_launch = true
+## will not give public ip when an instance is launched if map_ip is not enabled
 tags = {
     Name = "roboshop-${var.ENV}-pub-subnet-${element(var.AZ, count.index)}"
 }
