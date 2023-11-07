fa04deb8d1684fd958b60e6f609d043ab713a08b
Sush <sushcloud12@gmail.com>
2023-11-07 08:25:11 +0000

    i

--- peering.tf
+++ peering.tf
@@ -3,4 +3,8 @@ resource "aws_vpc_peering_connection" "peer" {
   vpc_id      = var.DEFAULT_VPC_ID
   peer_vpc_id = aws_vpc.main.id
   auto_accept = true
+
+   tags = {
+    Name = "roboshop-AWS-peering"
+  }
 }
