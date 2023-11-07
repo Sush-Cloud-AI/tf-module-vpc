134ca7394ae67bc40b791d5e6e07028fa5c56cff
Sush <sushcloud12@gmail.com>
2023-11-07 08:22:44 +0000

    i

--- peering.tf
+++ peering.tf
@@ -1,5 +1,5 @@
 # creates peering between aws network and private network
-resource "aws_vpc_peering_connection" "foo" {
+resource "aws_vpc_peering_connection" "peer" {
   vpc_id      = var.DEFAULT_VPC_ID
   peer_vpc_id = aws_vpc.main.id
   auto_accept = true
